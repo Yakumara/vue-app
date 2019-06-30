54579df4a6d4f29f57bd591d78e209d7da6d1f7b
Yakumara <pavel.mametev@gmail.com>
2019-07-01 03:44:57 +0700

    add select film or serial, add input for title&descr

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,51 @@
     section
       .container
         h1.ui-title-1 Главная
+        input(
+          type="text"
+          placeholder="Что будем смотреть?"
+          v-model="taskTitle"
+          @keyup.enter="newTask"
+        )
+        textarea(
+          type="text"
+          placeholder="Про что кинцо?"
+          v-model="taskDescription"
+          @keyup.enter="newTask"
+        )
+        .option-list
+          input.what-watch--radio(
+            type="radio"
+            id="radioFilm"
+            value="Фильм"
+            v-model="whatWatch"
+          )
+          label(
+            for="radioFilm"
+          ) Фильм
+          input.what-watch--radio(
+            type="radio"
+            id="radioSerial"
+            value="Сериал"
+            v-model="whatWatch"
+          )
+          label(
+            for="radioSerial"
+          ) Сериал
+        .total_time
+          .total_time__film(
+            v-if="whatWatch=='Фильм'"
+          )
+            span Время фильма
+          .total_time__serial(
+            v-if="whatWatch=='Сериал'"
+          )
+            span Время сериала
+          .tag_list
+            .ui-tag__wrapper
+              .ui-tag
+                span.tag-title GOT
+                .button-close
     section
       .container
         .task-list
@@ -18,7 +63,7 @@
                   span Total Time
                 span.button-close
               .task-item__content
-                .task-item__header  
+                .task-item__header
                   .ui-checkbox-wrapper
                     input.ui-checkbox(
                       type='checkbox'
@@ -29,28 +74,54 @@
                   p.ui-text-regular {{ task.description }}
 </template>
 <script>
-import { isFlowBaseAnnotation } from 'babel-types';
+import { isFlowBaseAnnotation } from 'babel-types'
 export default {
   data () {
     return {
+      taskId: 3,
+      taskTitle: '',
+      taskDescription: '',
+      whatWatch: 'Сериал',
       tasks: [
         {
           'id': 1,
-          'title': "Игра престолов",
-          'description': "К концу подходит время благоденствия, и лето, длившееся почти десятилетие, угасает. Вокруг средоточия власти Семи королевств, Железного трона, зреет заговор, и в это непростое время король решает искать поддержки у друга юности Эддарда Старка. В мире, где все — от короля до наемника — рвутся к власти, плетут интриги и готовы вонзить нож в спину, есть место и благородству, состраданию и любви. Между тем, никто не замечает пробуждение тьмы из легенд далеко на Севере — и лишь Стена защищает живых к югу от нее.",
+          'title': 'Игра престолов',
+          'description': 'К концу подходит время благоденствия, и лето, длившееся почти десятилетие, угасает. Вокруг средоточия власти Семи королевств, Железного трона, зреет заговор, и в это непростое время король решает искать поддержки у друга юности Эддарда Старка. В мире, где все — от короля до наемника — рвутся к власти, плетут интриги и готовы вонзить нож в спину, есть место и благородству, состраданию и любви. Между тем, никто не замечает пробуждение тьмы из легенд далеко на Севере — и лишь Стена защищает живых к югу от нее.',
           'whatWatch': 'Сериал',
           'completed': false,
           'editing': false
         },
         {
           'id': 2,
-          'title': "Мстители. Война без конечностей :)",
-          'description': "Пока Мстители и их союзники продолжают защищать мир от различных опасностей, с которыми не смог бы справиться один супергерой, новая угроза возникает из космоса: Танос. Межгалактический тиран преследует цель собрать все шесть Камней Бесконечности - артефакты невероятной силы, с помощью которых можно менять реальность по своему желанию. Всё, с чем Мстители сталкивались ранее, вело к этому моменту – судьба Земли никогда ещё не была столь неопределённой.",
+          'title': 'Мстители. Война без конечностей :)',
+          'description': 'Пока Мстители и их союзники продолжают защищать мир от различных опасностей, с которыми не смог бы справиться один супергерой, новая угроза возникает из космоса: Танос. Межгалактический тиран преследует цель собрать все шесть Камней Бесконечности - артефакты невероятной силы, с помощью которых можно менять реальность по своему желанию. Всё, с чем Мстители сталкивались ранее, вело к этому моменту – судьба Земли никогда ещё не была столь неопределённой.',
           'whatWatch': 'Фильм',
           'completed': false,
           'editing': false
-        },
+        }
       ]
+    }
+  },
+  methods: {
+    newTask () {
+      if(this.taskTitle==''){
+        return
+      }else{
+      this.tasks.push({
+        id: 3,
+        title: this.taskTitle,
+        description: this.taskDescription,
+        whatWatch: this.whatWatch,
+        completed: false,
+        editing: false
+      })
+
+      //Reset 
+      this.taskId +=1,
+      this.taskTitle = '',
+      this.taskDescription = ''
+      }
+      
     }
   }
 }
@@ -78,4 +149,12 @@ export default {
       margin-bottom 0
   .ui-checkbox-wrapper
     margin-right 8px
+  .option-list
+    display flex
+  .what-watch--radio
+    margin-right 15px
+  label
+    margin-right 20px
+    &:last-child
+      margin-right 0
 </style>
